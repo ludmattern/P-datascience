d7fec33e6ebc678b88817aff27bdc3ebaa2277cf
ludmattern <github.rfgrg@passmail.net>
2025-06-10 12:38:37 +0200

    misc

--- Data Science - 0 - Engineer/ex04/item_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE
-	IF NOT EXISTS item (
-		product_id INTEGER,
-		category_id BIGINT,
-		category_code VARCHAR(255),
-		brand VARCHAR(100)
-	);
-
-COPY item FROM '/item/item.csv' DELIMITER ',' CSV HEADER;
